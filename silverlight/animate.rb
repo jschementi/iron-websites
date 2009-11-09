7871160ed983ebee1a7a1b3aa65dcf8d95095884
Jimmy Schementi <jschementi@gmail.com>
2009-11-08 22:59:23 -0800

    Get rid of code-snippet rounding, and remove blog from homepage

--- silverlight/animate.rb
+++ silverlight/animate.rb
@@ -6,9 +6,7 @@
 codebox = document.send('code-snippet').children.first
 codebox.mouseover do |s, e|
   s.set_style_attribute 'width', '350px'
-  s.set_style_attribute 'backgroundColor', '#484848'
 end
 codebox.mouseout do |s, e|
   s.set_style_attribute 'width', '200px'
-  s.set_style_attribute 'backgroundColor', 'transparent'
 end
